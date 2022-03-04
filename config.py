81e17d4f38c0d29cbc551ff1d3e398035fda9f31
Mercy <mercycherotich757@gmail.com>
2022-03-04 19:08:22 +0300

    add email configurations

--- config.py
+++ config.py
@@ -1,8 +1,23 @@
+
+import os
 class Config:
     '''
     General configuration parent class
     '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'cherotichm23@gmail.com'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
 
@@ -13,7 +28,10 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/pitches'
+
+class TestConfig(Config):
+        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/pitches_test'
 
 
 class DevConfig(Config):
@@ -24,4 +42,13 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/pitches'
     DEBUG = True
+
+ 
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
